4232114d5dddadaac93ae57dcd84c548678602eb
happyball <yangchiyi10@gmail.com>
2015-05-15 15:11:49 +0800

    testing highcharts

--- js/compensate_index.js
+++ js/compensate_index.js
@@ -1,4 +1,5 @@
 var data_money;
+var sort_mode = 1;
 
 var row_contain = 20;
 
@@ -12,6 +13,7 @@ var y_count = 0;
 
 var org_list = ['交通部', '國防部', '財政部', '經濟部', '法務部', '教育部', '內政部', '行政院海巡署', '司法院', '行政院農委會', '外交部', '銓敘部', '考選部', '科技部', '衛生福利部', '國軍退除役官兵輔導委員會', '原住民族委員會'];
 var reason_list = ['交通意外', '天災', '軍訓事故', '行政疏失', '校園管教不當', '工程意外'];
+var sort_btn_list = ['date', 'money', 'class', 'reason'];
 
 var date_x_list = [];
 var date_y_list = [];
@@ -21,10 +23,14 @@ var class_x_list = [];
 var class_y_list = [];
 var reason_class_x_list = [];
 var reason_class_y_list = [];
+var reason_text_y_list = [];
 var class_list = {};
 var reason_class_list = {};
 var class_count = 1;
 var reason_class_count = 1;
+
+var all_org_event = [];
+var all_org_money = [];
 
 //-----------------------------------------------------------------------------------------//
 for(var i = 0; i < org_list.length; i++)
@@ -33,6 +39,48 @@ for(var i = 0; i < org_list.length; i++)
 for(var i = 0; i < reason_list.length; i++)
 	reason_class_list[reason_list[i]] = [];
 
+d3.csv("data/events_and_money.csv",function(data_event_money){
+	for ( var i = 0; i < data_event_money.length; i++ ){
+		var temp_event = {};
+		var temp_money = {};
+
+		temp_event['name'] = data_event_money[i]['org'];
+		temp_event['data'] = [];
+		temp_event['data'].push(parseInt(data_event_money[i]['events']));
+		all_org_event.push(temp_event);
+
+		temp_money['name'] = data_event_money[i]['org'];
+		temp_money['data'] = [];
+		temp_money['data'].push(parseInt(data_event_money[i]['money']));
+		all_org_money.push(temp_money);
+		
+	}
+
+	console.log(all_org_money);
+
+	$(function () { 
+    $('#all-money-chart').highcharts({
+        chart: {
+            type: 'column'
+        },
+        credits:{
+        	enabled: false
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['十年賠償總金額']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: all_org_money
+    });
+});
+});
 
 d3.csv("data/compensate_merge.csv", function(data){
 	/*console.log(data);
@@ -52,11 +100,41 @@ d3.csv("data/compensate_merge.csv", function(data){
 
 	tip = d3.tip().attr('class', 'd3-tip')
 			.offset(function(d){
-				return [200,10];
+				var this_x = 0;
+				if(sort_mode == 1)
+					this_x = date_x_list[d['id']];
+				else if(sort_mode == 2)
+					this_x = money_x_list[d['id']];
+				else if(sort_mode == 3)
+					this_x = class_x_list[d['id']];
+				else
+					this_x = reason_class_x_list[d['id']];
+
+
+				if(this_x + $(".d3-tip").width() + 250 > $(window).width())
+					return [200,-30];
+				else
+					return [200,10]; 
 			})
-			.direction(function(d){ return 'e'; })
+			.direction(function(d){
+				var this_x = 0;
+				if(sort_mode == 1)
+					this_x = date_x_list[d['id']];
+				else if(sort_mode == 2)
+					this_x = money_x_list[d['id']];
+				else if(sort_mode == 3)
+					this_x = class_x_list[d['id']];
+				else
+					this_x = reason_class_x_list[d['id']];
+
+
+				if(this_x + $(".d3-tip").width() + 250 > $(window).width())
+					return 'w';
+				else
+					return 'e'; 
+			})
 			.html(function(d) { 
-				var tip_html = "<div class = 'tip-little-title'>主管機關</div><div class = 'tip-text'><span class = 'tip-org-" + org2Num(d.org) + "'>●</span>" + d.org + "</div><div class = 'tip-little-title'>請求權人狀況</div><div class = 'tip-text'>"+ d['status'] +"</div><div class = 'tip-little-title'>撥款日期</div><div class = 'tip-text'>" + d.date + "</div><div class = 'tip-little-title'>賠償金額</div><div class = 'tip-text' id = 'tip-text-money'>" + modNum(d.money) + "</div><br><div class = 'tip-little-title'>案情摘要</div><div class = 'tip-text' id = 'tip-text-detail'>" + d.event_detail + "</div><div class = 'tip-little-title'>事件原因</div><div class = 'tip-text'>" + d.event_class + "</div>";
+				var tip_html = "<div class = 'tip-little-title'>主管機關</div><div class = 'tip-text'><span class = 'tip-org-" + org2Num(d.org) + "'>●</span>" + d.detail_org + "</div><div class = 'tip-little-title'>事件原因</div><div class = 'tip-text'>" + d.event_class + "</div><div class = 'tip-little-title'>死傷狀況</div><div class = 'tip-text'>"+ d['status'] +"</div><div class = 'tip-little-title'>撥款日期</div><div class = 'tip-text'>" + d.date + "</div><div class = 'tip-little-title'>賠償金額</div><div class = 'tip-text' id = 'tip-text-money'>" + modNum(d.money) + "</div><br><div class = 'tip-little-title'>案情摘要</div><div class = 'tip-text' id = 'tip-text-detail'>" + d.event_detail + "</div>";
 				return tip_html;
 			});
 	
@@ -113,6 +191,7 @@ d3.csv("data/compensate_merge.csv", function(data){
 		.on("mouseover", function(d){
 			// console.log(d.date);
 			tip.show(d);
+
 		})
 		.on("mouseout", function(d){
 			tip.hide(d);
@@ -184,6 +263,8 @@ d3.csv("data/compensate_merge.csv", function(data){
 
 		// console.log(class_x_list);
 		// console.log(class_y_list);
+		reason_text_y_list[0] = y_init;
+		y_now = y_init + y_dis;
 
 		for (var i = 0; i < Object.keys(reason_class_list).length; i++){
 			for (var j = 0; j < reason_class_list[reason_list[i]].length; j++){
@@ -192,23 +273,51 @@ d3.csv("data/compensate_merge.csv", function(data){
 
 				if(reason_class_count%row_contain != 0){
 					reason_class_x = x_init + ((reason_class_count%row_contain) - 1)*x_dis;
-					reason_class_y = y_init + Math.floor(reason_class_count/row_contain) * y_dis;
+					reason_class_y = y_now + Math.floor(reason_class_count/row_contain) * y_dis;
 				}
 				else{
 					reason_class_x = x_init + (row_contain - 1)*x_dis;
-					reason_class_y = y_init + (Math.floor(reason_class_count/row_contain) - 1) * y_dis;
+					reason_class_y = y_now + (Math.floor(reason_class_count/row_contain) - 1) * y_dis;
 				}
 
 				reason_class_x_list[reason_class_list[reason_list[i]][j]['id']] = reason_class_x;
 				reason_class_y_list[reason_class_list[reason_list[i]][j]['id']] = reason_class_y;
 
 				reason_class_count = reason_class_count + 1;
+
+				if(j == reason_class_list[reason_list[i]].length - 1){
+					if(i < Object.keys(reason_class_list).length - 1)
+						reason_text_y_list[i + 1] = reason_class_y + y_dis + 30;
+					y_now = reason_class_y + y_dis*2;
+				}
 			}
+
+			reason_class_count = 1;
+
 		}
 
+		for (var i = 0; i < reason_list.length; i++){
+			svg.append("text").text(reason_list[i])
+			.attr({
+				'class': "reason-text",
+				'x': 30,
+				'y': reason_text_y_list[i],
+				'fill': "#ededed"
+			});
+		}
 });
 
 $("#sort-by-date").click(function(){
+	if(sort_mode == 4){
+		$("svg").attr("height", 1800);
+		$("text").fadeOut(1000);
+	}
+
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "none");
+
+	sort_mode = 1;
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "2px solid #ededed");
+
 	for(var i = 1; i <= date_x_list.length; i++){
 		d3.select('#circle-id-' + i)
 		.transition().duration(1000)
@@ -217,9 +326,20 @@ $("#sort-by-date").click(function(){
 			'cy': date_y_list[i]
 		});
 	}
+	
 });
 
 $("#sort-by-money").click(function(){
+	if(sort_mode == 4){
+		$("svg").attr("height", 1800);
+		$("text").fadeOut(1000);
+	}
+
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "none");
+
+	sort_mode = 2;
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "2px solid #ededed");
+
 	for(var i = 1; i <= money_x_list.length; i++){
 		d3.select('#circle-id-' + i)
 		.transition().duration(1000)
@@ -231,6 +351,16 @@ $("#sort-by-money").click(function(){
 });
 
 $("#sort-by-class").click(function(){
+	if(sort_mode == 4){
+		$("svg").attr("height", 1800);
+		$("text").fadeOut(1000);
+	}
+
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "none");
+
+	sort_mode = 3;
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "2px solid #ededed");
+	
 	for(var i = 1; i <= class_x_list.length; i++){
 		d3.select('#circle-id-' + i)
 		.transition().duration(1000)
@@ -242,6 +372,14 @@ $("#sort-by-class").click(function(){
 });
 
 $("#sort-by-reason").click(function(){
+	$("svg").attr("height", 2450);
+	$("text").fadeIn(1000);
+
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "none");
+
+	sort_mode = 4;
+	$("#sort-by-" + sort_btn_list[sort_mode - 1]).css("border-bottom", "2px solid #ededed");
+
 	for(var i = 1; i <= class_x_list.length; i++){
 		d3.select('#circle-id-' + i)
 		.transition().duration(1000)
@@ -251,6 +389,8 @@ $("#sort-by-reason").click(function(){
 		});
 	}
 });
+
+
 
 function org2Num(str){
 	switch(str){
